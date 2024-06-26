ce07a24bd97f27880c1bebcfba9d5c3d3c6efb61
Carsten <lccniemand@gamil.com>
2024-06-27 00:09:29 +0200

    Nailed all the challenges in the homework section of this - video_123... I will do the solutions video tomorrow.

--- Lessons/video_123_web_Scraping_exercise.py
+++ Lessons/video_123_web_Scraping_exercise.py
@@ -54,4 +54,62 @@ print(f"Top Ten Tags: {top_ten_tags}")
 # Get all the unique authors - use a set. (Duplicates not allowed!)
 # So, first just try to do it for one page, then look back at the for loop in the book part to scrape tutorial to see how to jump through pages. This will be plenty simple.
 
+# Things to try:
+# if " No quotes found! " --> stop loop or something
+
+# Also maybe try to check if the .col-md-8 has div.quote, if not - stop loop or something
+
+# Ended up going with if there is a class on the page called "next", increment the number, else break.
+
+# Here is a basic regex to find the last item in the current url... so we will start it at 1, then increment it....
+import re
+# search_pattern = r"\d+"
+# current_page = re.findall(search_pattern, current_url[:-1])
+# current_page_num = str(current_page[0]) # is int initially because we need to compare it with i... maybe we can keep it a string but then do something like if i < int(current_page_num) : i += 1... 
+# but no... What I want to do is to always keep i one less than the current page num
+# print(current_page_num)
+# new_res = requests.get(current_url)
+# new_soup = bs4.BeautifulSoup(new_res.text, "lxml")
+# select_author_from_soup = new_soup.select(".author")
+# check_next = new_soup.select(".next")
+# def next_checker(check_this) :
+#     if check_this :
+#         return True
+#     else :
+#         return False
+# print(f"select_author_from_soup: {select_author_from_soup}")
+
+def find_all_authors() :
+    current_url ="https://quotes.toscrape.com/page/{}/" 
+    all_author_set = set() 
+    i = 0
+    while i <= i + 1 :
+        i += 1
+        res = requests.get(current_url.format(i))
+        soup = bs4.BeautifulSoup(res.text, "lxml") 
+        if soup.select(".next") :
+            q_num = 0
+            for author in soup.select(".author") :
+                    the_author = soup.select(".author")[q_num].get_text()
+                    all_author_set.add(the_author)
+                    q_num += 1
+        else :
+            # have to loop it once more for the last page... THEN break, otherwise we miss the entire last page.
+            # There must be a better way than this
+            q_num = 0
+            for author in soup.select(".author") :
+                    the_author = soup.select(".author")[q_num].get_text()
+                    all_author_set.add(the_author)
+                    q_num += 1
+            break
+        
+    all_author_list = list(all_author_set)
+    return(sorted(all_author_list))
+
+print("Challenge 5 - Find all unique Authors on the Page! - DONE \m/")
+for i in find_all_authors() :
+     print(i)
+
+# Hell Yea! I nailed this one! Took me long enough - but I nailed it! - No thanks, whatsoever to online resources
+# See what I did by looping the for author in soup.select(".author") : -> then I create the_author?... That will also work to complete challenge 4 "properly..."
 
